f2f14b93614068c217c172038bb49d1718fac555
tharunoptimus <tharunoptimus@outlook.com>
2023-04-08 00:26:17 +0530

    Chunk Maker 1 Array to 5

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ def get_transcript(video_id):
             newArray.append(item["text"])
         return newArray
     return array_fi(srt)
+
+def get_sentences(text, num_of_sentences=5):
+    length = len(text)
+    length_of_each_part = length // num_of_sentences
+    new_array = []
+    for i in range(0, 5):
+        new_array.append(text[i*length_of_each_part:(i+1)*length_of_each_part])
+    return new_array
