ac31038c4e70068889f7d8451ba12ff19db62968
tharunoptimus <tharunoptimus@outlook.com>
2023-04-10 04:44:31 +0530

    Remove unwanted comments

--- app.py
+++ app.py
@@ -57,13 +57,6 @@ def get_responses(array):
 
 app = Flask(__name__)
 
-# @app.after_request
-# def after_request(response):
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-#     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-#     return response
-
 @app.route('/', methods=['POST'])
 @cross_origin()
 def api():
